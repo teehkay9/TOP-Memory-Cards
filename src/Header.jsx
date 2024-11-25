419eb20d318996e12550f9f8ce540ff14218c4ed
Thomas <thomashoelsken@yahoo.de>
2024-11-25 13:03:35 +0100

    Add styling
    - added styling to the components Header & App

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ export default function Header({ score, highScore }) {
   return (
     <header className="app__header">
       <section className="header__description">
-        <h1>Pokemon Memory Game</h1>
+        <h1 className="app__header-title">Pokemon Memory Game</h1>
         <p>Get points by clicking on an image but don&#39;t click on any more than once!</p>
       </section>
       <Scoreboard score={score} highScore={highScore} />
